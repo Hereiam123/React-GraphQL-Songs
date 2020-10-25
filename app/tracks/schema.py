936e63029caf5806e8d99e4e2b81aa0e1eef46b4
hereiam123 <bdemaio1@gmail.com>
2020-10-25 18:03:37 -0400

    Adding in updating a track, if user who created it

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -36,5 +36,30 @@ class CreateTrack(graphene.Mutation):
         return CreateTrack(track=track)
 
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, title, description, url, track_id):
+        user = info.context.user
+        track = Track.objects.get(id=track_id)
+
+        if track.posted_by != user:
+            raise Exception("Not permitted to update this track.")
+
+        track.title = title
+        track.description = description
+        track.url = url
+
+        track.save()
+        return UpdateTrack(track=track)
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
