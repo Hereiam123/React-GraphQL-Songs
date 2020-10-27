3b0c8b2ca9be84d20c79f7b0ffac3ae761fc4efa
hereiam123 <bdemaio1@gmail.com>
2020-10-27 08:00:19 -0400

    Adding in React front end client to get queries

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,17 +1,14 @@
 import React from "react";
 import withRoot from "./withRoot";
-import { Query } from "react-apollo";
+import { useQuery } from "react-apollo";
 import { gql } from "apollo-boost";
 
-const Root = () => (
-  <Query query={GET_TRACKS_QUERY}>
-    {({ data, loading, error }) => {
-      if (loading) return <div>Loading</div>;
-      if (error) return <div>Error</div>;
-      return <div>{JSON.stringify(data)}</div>;
-    }}
-  </Query>
-);
+const Root = () => {
+  const { loading, error, data } = useQuery(GET_TRACKS_QUERY);
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error</div>;
+  return <div>{JSON.stringify(data)}</div>;
+};
 
 const GET_TRACKS_QUERY = gql`
   {
