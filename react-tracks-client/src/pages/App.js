97ea96b5a6a2846433f10091b2a3dc16efd9b814
hereiam123 <bdemaio1@gmail.com>
2020-10-28 08:25:30 -0400

    Connecting loading in track data into a TrackList

--- react-tracks-client/src/pages/App.js
+++ react-tracks-client/src/pages/App.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 import { makeStyles } from "@material-ui/core/styles";
+import SearchTracks from "../components/Track/SearchTracks";
+import TrackList from "../components/Track/TrackList";
+import CreateTrack from "../components/Track/CreateTrack";
+import Loading from "../components/Shared/Loading";
+import Error from "../components/Shared/Error";
 
 const App = () => {
-  const classes = useStyles();
-  return <div>App</div>;
+  const classes = useStyles(GET_TRACKS_QUERY);
+  const { loading, error, data } = useQuery(GET_TRACKS_QUERY);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  return (
+    <div className={classes.container}>
+      <SearchTracks />
+      <CreateTrack />
+      <TrackList tracks={data.tracks} />
+    </div>
+  );
 };
+
+const GET_TRACKS_QUERY = gql`
+  query getTracksQuery {
+    tracks {
+      id
+      title
+      description
+      url
+      likes {
+        id
+      }
+      postedBy {
+        id
+        username
+      }
+    }
+  }
+`;
 
 const useStyles = makeStyles((theme) => ({
   container: {
