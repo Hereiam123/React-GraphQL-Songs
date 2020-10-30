336ecca4c0b4b7dc4bcbe56c503aeb6c115692eb
hereiam123 <bdemaio1@gmail.com>
2020-10-30 18:49:02 -0400

    Converting over DB from sqlite to postgres

--- app/app/settings.py
+++ app/app/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tracksDB',
+        'USER': 'postgres',
+        'PASSWORD': 'bdem',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
