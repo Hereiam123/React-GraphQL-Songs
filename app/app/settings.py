d2524da0ed68d7faf515841c075320f8e4d4d704
hereiam123 <bdemaio1@gmail.com>
2020-11-01 03:51:13 -0500

    Dev save

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
