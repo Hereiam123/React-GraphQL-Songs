b190554ef23d91723878b6633eed155e1fcb4d99
hereiam123 <bdemaio1@gmail.com>
2020-11-04 01:28:26 -0500

    All the updates

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tracksDB',
+        'NAME': 'reactracks',
         'USER': 'postgres',
         'PASSWORD': 'bdem',
         'HOST': 'localhost',
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 print(os.path.join(os.path.dirname(BASE_DIR), 'react-tracks-client'))
 
 REACT_APP_DIR = os.path.join(os.path.dirname(BASE_DIR), 'react-tracks-client')
--- app/app/urls.py
+++ app/app/urls.py
@@ -24,6 +24,5 @@ from graphene_file_upload.django import FileUploadGraphQLView
 urlpatterns = [
     path('', csrf_exempt(FrontendAppView.as_view())),
     path('admin/', admin.site.urls),
-    url(r'^graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
-    # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    url(r'^graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True)))
 ]
