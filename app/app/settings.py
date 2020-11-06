f3a3d1cc67fb8a5f8f5e25f0f74ffef82f46bb73
hereiam123 <bdemaio1@gmail.com>
2020-11-05 21:23:30 -0500

    Updating files

--- app/app/settings.py
+++ app/app/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build'),
+    os.path.join(BASE_DIR, '../react-tracks-client/build')
 ]
