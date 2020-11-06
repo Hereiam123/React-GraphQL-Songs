a933d73c9a20ff17b2a02645ccd8954b76dc415b
hereiam123 <bdemaio1@gmail.com>
2020-11-05 20:30:12 -0500

    Updating files

--- app/app/settings.py
+++ app/app/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
        'NAME': 'postgres',
        'USER': 'postgres',
-       'HOST': 'db',
-       'PORT': '5432',
+       'HOST': 'localhost',
+       'PORT': '',
    }
 }
 
