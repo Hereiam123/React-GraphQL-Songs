8ab0ea0b52651930f37ea00cae20fc3e9699cece
Brian De Maio <briandemaio@Brians-MacBook-Air.local>
2021-12-08 18:25:55 -0500

    Trying to fix initial user login bug crash

--- app/users/schema.py
+++ app/users/schema.py
@@ -18,7 +18,6 @@ class Query(graphene.ObjectType):
 
     def resolve_me(self, info):
         user = info.context.user
-        print(user.is_authenticated)
         if not user.is_authenticated:
             raise GraphQLError("Not logged in!")
 
