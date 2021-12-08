c2c91fe00b644d86fdabb6f499f939edecd6fb6c
Brian De Maio <briandemaio@Brians-MacBook-Air.local>
2021-12-08 09:52:17 -0500

    Update allowed hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '143.198.185.124', 'reactsongs.com', 'www.reactsongs.com',"*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '143.198.185.124', 'reactsongs.com', 'www.reactsongs.com',"*","app"]
 
 # Application definition
 
