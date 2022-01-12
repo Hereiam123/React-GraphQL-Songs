d76c85b9b77e1dc9cac353f7b97e3f8114682c05
Brian De Maio <bdemaio1@gmail.com>
2022-01-12 00:18:14 -0500

    Rerouting authenticated user to tracks page by default if requesting home route

--- react-tracks-client/src/components/Auth/index.js
+++ react-tracks-client/src/components/Auth/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_ME_QUERY } from "../../sharedQueries";
 import withRoot from "../../withRoot";
 import Login from "./Login";
 import Register from "./Register";
+import Loading from "./Loading";
+import Error from "./Error";
 
 export default withRoot(({ history }) => {
   const [isLogin, setIsLogin] = useState(false);
