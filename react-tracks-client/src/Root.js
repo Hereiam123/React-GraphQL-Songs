689b92d8070a897cd29cb914a4d2bbba96138941
Brian De Maio <bdemaio1@gmail.com>
2022-01-11 23:59:43 -0500

    Rerouting authenticated user to tracks page by default if requesting home route

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,6 +1,6 @@
 import React from "react";
 import withRoot from "./withRoot";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import App from "./pages/App";
 import Profile from "./pages/Profile";
 import Header from "./components/Shared/Header";
@@ -12,6 +12,9 @@ const Root = ({ data }) => {
     <UserContext.Provider value={data.me}>
       <Header currentUser={data.me} />
       <Switch>
+        <Route path="/" exact>
+          <Redirect to="/tracks" />
+        </Route>
         <Route path="/tracks" exact component={App} />
         <Route path="/profile/:id" component={Profile} />
         <Route component={NotFound} />
