b70edc81c656be5b5bce5dc704b7af0403a01394
Brian De Maio <bdemaio1@gmail.com>
2022-01-12 00:15:13 -0500

    Rerouting authenticated user to tracks page by default if requesting home route

--- react-tracks-client/src/components/Auth/index.js
+++ react-tracks-client/src/components/Auth/index.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import withRoot from "../../withRoot";
 import Login from "./Login";
 import Register from "./Register";
 
 export default withRoot(({ history }) => {
   const [isLogin, setIsLogin] = useState(false);
+  const { loading, error, data } = useQuery(GET_ME_QUERY, {
+    fetchPolicy: "cache-and-network",
+  });
+  if (loading) return <Loading />;
+  if (error) {
+    return <Error error={error.toString()} />;
+  }
+  if (data) {
+    return <Redirect to="/tracks" />;
+  }
   return isLogin ? (
     <Login setIsLogin={setIsLogin} history={history} />
   ) : (
